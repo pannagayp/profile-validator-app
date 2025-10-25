f222160972bef2d562a392c2d8ff3c84b7568782
Pannaga Y.P <pannagayp282@gmail.com>
2025-10-25 16:40:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9836957865-55e9c",
+  "appId": "1:985743849745:web:7abb343680242252d934b0",
+  "apiKey": "AIzaSyA9W8vzABaCuPETd_BjOwTPZSFE4KPIdPk",
+  "authDomain": "studio-9836957865-55e9c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "985743849745"
+};
